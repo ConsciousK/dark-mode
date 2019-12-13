ed5b8efcaf6182a21b92e728355bc3e0bb465a56
Kevin Grant <ConsciousK@yahoo.com>
2019-12-12 19:52:01 -0500

    Yarn installed and local storage hook created

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+    return window.localStorage.getItem(key) || initialValue;
+  });
+
+  const setValue = value => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, value);
+  };
+
+  return [storedValue, setValue]}
